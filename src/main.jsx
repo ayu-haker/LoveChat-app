02bd3a16ad977cb54ca09c49643bfb2495c7bb87
ayushman bosu roy <ayushmanbosuroy@gmail.com>
2025-06-21 02:24:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppContext from "./context/AppContext";
 import AppContextProvider from "./context/AppContext";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
